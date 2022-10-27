25182e4fa7fdeca999f5957b335cfd26183134ad
erickxbliv <92802590+erickxbliv@users.noreply.github.com>
2022-10-27 15:38:45 -0300

    Add files via upload

---
+++ funcoes.py
@@ -0,0 +1,14 @@
+def construir(lista):
+
+    #receber como parametro se e um elevador, e se for uma sala, qual o tamanho dela
+    #um dos parametros ja e qual a imagem que vai ser usada
+
+    contagem = 0
+    while contagem < 147:
+        if lista[contagem].pedra == False:
+            if lista[contagem].vazio == True:
+                #se for um elevador, printar nos pretendentes totais e verticais
+                #se for uma sala, imprima apenas nos totais e correspondente ao tamanho da sala, claro, só quando couber
+                pass
+
+#animacao ficar aqui?
