db825e9fa6c2c458f5d4ac906eaa42011209ab77
erickxbliv <92802590+erickxbliv@users.noreply.github.com>
2022-10-27 15:37:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,30 @@ import pygame
 import sys
 import celulas
 from os import path
-import menu
+import funcoes
 
 pygame.init()
 
 def animacao():
     pass
 
+#compensa mais não mostrar a pretendencia quando se tem pedra, do que ter uma função pra atualizar as pedras quebradas e outra pra 
+
 tamanho = largura, altura = 1071, 651
 janela = pygame.display.set_mode(tamanho)
 
 fundo = pygame.image.load(path.join('cenario', 'dia.png'))
 #fundo_noite = pygame.image.load(path.join('cenario', 'noite.png'))
-configuracoes = pygame.image.load("configuracoes.png")
+configuracoes = pygame.image.load("configuracoes.png")   #pode ficar "alocado" na celula 1 (0,0), e assim se sabe se foi aberta. 
 
+#sera se compensava eu ter os dados do jogo numa classe?
 coordenadas = w, z = 0, 0
 preto = 0, 0, 0
 dinheiro = 999999
 horario = 1
-sistema_atual = "espectador"
+sistema_atual = "espectador"        #modos: espectador - visualizar o bunker, construir - visualizar onde construir, espiar - assistir uma sala individualmente
 achei = False
 #clock tick
-
 
 matriz_grid = []
 lista = []
@@ -34,6 +36,7 @@ while True:
         if event.type == pygame.QUIT: sys.exit()
         if event.type == pygame.MOUSEBUTTONUP:
             position = pos_x, pos_y = pygame.mouse.get_pos()
+            #se clicar numa sala, tem que abrir ela
             
             if sistema_atual == "espectador":
                 x = 1020
@@ -50,23 +53,23 @@ while True:
                 x = (x//51) + 1
                 pos_vetor = (x + y) - 1
 
-
-                print(pos_vetor)
+                #print(pos_vetor)
 
                 if celulas.lista[pos_vetor].pedra == True:
                     #remocao = tirar_pedra()
                     remocao = True
                     if remocao == True:
-                        y = (y // 21) * 93
+                        y = (y // 21) * 93  #pegando o y de volta, pois quanto mais embaixo mais caro
                         dinheiro = dinheiro - ((y * 50) + 50)
                         celulas.lista[pos_vetor].pedra = False
+                        #atualizar_pretendencia_d1_celula(pos_vetor,lista)
 
-
-                        print(celulas.lista[pos_vetor].pedra)#dando um erro aqui, havia uma pedra no ide 66 ou posv 65, mas so foi no 86
+                        #print(celulas.lista[pos_vetor].pedra)
 
                         #verificar_pretendencia
-
-
+            elif sistema_atual == "construir":
+                #selecionar qual comprar atraves de um menu, sera se isso tambem deveria ser uma classe?
+                funcoes.construir()
 
 
     janela.fill(preto)
@@ -82,4 +85,4 @@ while True:
         contagem += 1
 
     pygame.display.flip()
-    
+    #pygame.time.delay(60)
